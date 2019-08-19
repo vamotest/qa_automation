8eb7d313d6655e8aa6060bdb96842c702e9c998d
Valery Moiseev <vamotest@gmail.com>
2019-08-19 17:00:34 +0300

    Add pytest intro

---
+++ 02_Pytest_introduction/8_homework/python_types.py
@@ -0,0 +1,3 @@
+def sum_integers(int1, int2):
+    # type: (integer, integer) -> integer
+    return int1 + int2
---
+++ 02_Pytest_introduction/conftest.py
@@ -0,0 +1,5 @@
+import pytest
+
+# @pytest.fixture()
+# def first_fixture():
+#     print("\nPrint from 'first_fixture' in conftest.py root")
