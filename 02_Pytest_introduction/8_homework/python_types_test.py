fb8fa8284ef392a82c48683ee5e1ac8f866f5947
Valery Moiseev <vamotest@gmail.com>
2019-08-23 19:56:33 +0300

    Add test_string

--- 02_Pytest_introduction/8_homework/python_types_test.py
+++ 02_Pytest_introduction/8_homework/python_types_test.py
@@ -1,4 +1,4 @@
-import python_types
+from python_types import sum_integers, string
 
 
 class TestPythonExamples:
@@ -8,9 +8,17 @@ class TestPythonExamples:
 
     def test_integers(self, sum_integers_data):
         """
-        Test checks Python data type "integer"
+        Test checks Python data type "Integer"
         :param sum_integers_data:
         :return: sum of two integers
         """
-
         assert sum_integers(*sum_integers_data) == 9
+
+    def test_string(self, string_data):
+        """
+        Test checks Python data type "String"
+        :param string_data:
+        :return: is palindrome string or not
+        """
+
+        assert string(string_data) == 'Yes'
