a9eb4ff6aa60fdc955d23e76b2dc2a455973c324
Valery Moiseev <vamotest@gmail.com>
2019-09-05 18:31:37 +0300

    Add selenium test

---
+++ 05_Selenium_intro/test_selenium.py
@@ -0,0 +1,12 @@
+
+def test_start_driver(chosen_driver, opencart_url):
+    """
+    Проверяем, что в выбранном или дефолтном браузере открывается
+    запрашиваемая или дефолтная страница и находится заголовок 'Contact Us'
+    :param
+    chosen_driver = default or user browser,
+    opencart_url = default or user url
+    """
+    chosen_driver.get(opencart_url)
+    assert chosen_driver.title == 'Your Store'
+    chosen_driver.close()
