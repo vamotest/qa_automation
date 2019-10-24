b9a2fde6de59142dea22a88d9c70dd296fb2b70d
Valery Moiseev <vamotest@gmail.com>
2019-10-24 14:31:59 +0300

    Add  ssh client logic

---
+++ 21_application_layer_protocols/Networking/ssh_client.py
@@ -0,0 +1,20 @@
+import yaml
+import paramiko
+
+
+conf = yaml.safe_load(open('configuration.yml'))
+client = paramiko.SSHClient()
+
+
+def ssh_authorization():
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(
+        hostname=conf['credentials']['hostname'],
+        username=conf['credentials']['username'],
+        password=conf['credentials']['password'],
+        port=conf['credentials']['port']
+    )
+
+
+def ssh_client_finish():
+    client.close()
