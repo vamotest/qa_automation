d3a60d402b344b9f897c80c680e9441bcc1d013a
Valery Moiseev <vamotest@gmail.com>
2019-11-02 00:19:41 +0300

    Add basic Chrome, Firefox, Safari

--- 05_Selenium_intro/conftest.py
+++ 05_Selenium_intro/conftest.py
@@ -1,19 +1,58 @@
+import pytest
 from selenium import webdriver
-import time
-
-chrome = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
-chrome.get("https://yandex.ru/")
-time.sleep(1)
-chrome.quit()
-
-firefox = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
-firefox.get("https://yandex.ru/")
-time.sleep(1)
-firefox.quit()
 
 
-safari = webdriver.Safari(executable_path='/usr/bin/safaridriver')
-safari.get("https://yandex.ru/")
-time.sleep(1)
-safari.quit()
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser",
+        action="store",
+        default="chrome",
+        help="This is request browser",
+        required=False
+    )
 
+    parser.addoption(
+        "--opencart_url",
+        action="store",
+        default="http://localhost:80/",
+        help="This is opencart_url",
+        required=False
+    )
+
+
+@pytest.fixture()
+def browser_param(request):
+    return request.config.getoption("--browser")
+
+
+@pytest.fixture()
+def opencart_url_param(request):
+    return request.config.getoption('--opencart_url')
+
+
+@pytest.fixture()
+def chosen_driver(browser_param):
+    if browser_param == "Chrome":
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        driver = webdriver.Chrome(options=options)
+        driver.maximize_window()
+        return driver
+    elif browser_param == "Firefox":
+        options = webdriver.FirefoxOptions()
+        options.add_argument("--headless")
+        driver = webdriver.Firefox(options=options)
+        driver.maximize_window()
+        return driver
+    elif browser_param == "Safari":
+        """
+        У Safari нет headless-режима
+        """
+        driver = webdriver.Safari()
+        driver.maximize_window()
+        return driver
+
+
+@pytest.fixture()
+def opencart_url(opencart_url_param):
+    return opencart_url_param
