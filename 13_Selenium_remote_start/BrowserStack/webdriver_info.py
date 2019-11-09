6d1ab6ba25fc9e22807a91910c152094c6531625
Valery Moiseev <vamotest@gmail.com>
2019-11-09 18:22:20 +0300

    Remove test, add def remote_driver

--- 13_Selenium_remote_start/BrowserStack/webdriver_info.py
+++ 13_Selenium_remote_start/BrowserStack/webdriver_info.py
@@ -1,28 +1,21 @@
-import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-desired_cap = {
- 'browser': 'Firefox',
- 'browser_version': '70.0 beta',
- 'os': 'Windows',
- 'os_version': '10',
- 'resolution': '2048x1536',
- 'name': 'Sample Test'
-}
 
-driver = webdriver.Remote(
-    command_executor='http://eubelov1:PhgpeViAK8knGySJZPfb@hub.browserstack.com:80/wd/hub',
-    desired_capabilities=desired_cap)
+def remote_driver():
 
-driver.maximize_window()
-driver.get("http://www.google.com")
-if not "Google" in driver.title:
-    raise Exception("Unable to load google page!")
-elem = driver.find_element_by_name("q")
-elem.send_keys("flip a coin")
-elem.submit()
-print(driver.title)
-time.sleep(5)
-driver.quit()
+    desired_capabilities = {
+        'browser': 'Firefox',
+        'browser_version': '70.0 beta',
+        'os': 'Windows',
+        'os_version': '10',
+        'resolution': '2048x1536',
+        'name': 'Sample Test'
+    }
+
+    driver = webdriver.Remote(
+        command_executor=
+        'http://eubelov1:PhgpeViAK8knGySJZPfb@hub.browserstack.com:80/wd/hub',
+        desired_capabilities=desired_capabilities
+    )
+
+    return driver
