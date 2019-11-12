21c3a7270994dd7a97f0bb68e0ccdf87a17b9625
Valery Moiseev <vamotest@gmail.com>
2019-11-12 22:20:59 +0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qa_automation',
+    version='0.1',
+    url='https://github.com/vamotest/qa_automation',
+    license='MIT',
+    author='vamotest',
+    author_email='vamotest@gmail.com',
+    description='Python QA Engineer',
+    packages=find_packages(exclude=['test']),
+    install_requires=[
+        'pandas==0.25.3',
+        'paramiko==2.6.0',
+        'pytest==4.6.0',
+        'pyyaml==5.1.2',
+        'selenium==3.141.0'
+    ],
+    zip_safe=False
+)
