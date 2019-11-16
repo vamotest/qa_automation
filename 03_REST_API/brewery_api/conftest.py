8ed8a36d348d91df446a235fc26878fd49bcb2d0
Valery Moiseev <vamotest@gmail.com>
2019-11-16 22:04:49 +0300

    Add conftest

---
+++ 03_REST_API/brewery_api/conftest.py
@@ -0,0 +1,32 @@
+import requests
+import pytest
+from variables import breweries_url
+
+
+@pytest.fixture()
+def response_brewery_list():
+    """
+    Фикстура возвращает результат запроса получения списка пивоварен
+    """
+    response = requests.get(breweries_url).json()
+    return response
+
+
+@pytest.fixture()
+def state_url():
+    """
+    Фикстура возвращает ссылку пивоварен по штату
+    """
+    state_url = f'{breweries_url}' + '?by_state='
+    return state_url
+
+
+@pytest.fixture(params=[{"by_tag": "patio"}])
+def response_brewery_by_tag(request):
+    """
+    Фикстура возвращает список пивоварен, выбранных по тэгу
+    """
+    response = requests.get(breweries_url, params=request.param).json()
+    return response
+
+
