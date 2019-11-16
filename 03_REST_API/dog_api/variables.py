d88f32c92c03025f40e56b6b80bcdde8d9246dea
Valery Moiseev <vamotest@gmail.com>
2019-11-16 21:56:06 +0300

    Add variables

---
+++ 03_REST_API/dog_api/variables.py
@@ -0,0 +1,8 @@
+# Url произвольного изображения породы собаки:
+random_image_url = 'https://dog.ceo/api/breeds/image/random'
+
+# Url породы собаки:
+concrete_breed_url = 'https://dog.ceo/api/breed/'
+
+# Url списка всех пород собак:
+all_breeds_url = 'https://dog.ceo/api/breeds/list/all'
