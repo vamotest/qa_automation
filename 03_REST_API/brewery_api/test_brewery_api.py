212afe983837838686228be6ec8195c165147900
Valery Moiseev <vamotest@gmail.com>
2019-11-22 15:46:05 +0300

    Remove if-elif-else logic

--- 03_REST_API/brewery_api/test_brewery_api.py
+++ 03_REST_API/brewery_api/test_brewery_api.py
@@ -9,62 +9,53 @@ class TestBrewery:
         """
         Тест проверяет, что пришел список пивоварен
         """
-        if response_brewery_list is not None:
-            print(response_brewery_list)
-        elif response_brewery_list is not None:
-            assert False, response_brewery_list
-        else:
-            assert False, 'Something wrong'
+        assert response_brewery_list is not None
 
     @pytest.mark.parametrize('state', ["Alabama", "Alaska", "Arkansas"])
     def test_brewery_by_state(self, state, state_url):
         """
         Тест проверяет по выбранному штату
         """
-        response_brewery_state = requests.get(state_url).json()
-        if state in response_brewery_state[0]['state']:
-            print(f'State: {state}')
-        elif state not in response_brewery_state[0]['state']:
-            assert False, f'State: {state}'
-        else:
-            assert False, 'Something wrong'
+        response_brewery_state = requests.get(state_url + f'{state}').json()
+
+        # Проверяем, что {state} есть в ответе на запрос в поле 'state'
+        assert state in response_brewery_state[0]['state']
 
     @pytest.mark.parametrize('name', [
-        "Trim Tab Brewing", "Yellowhammer Brewery", "Mudshark Brewing Co"])
-    def test_brewery_by_name(self, name):
+        "Trim Tab Brewing",
+        "Yellowhammer Brewery",
+        "Mudshark Brewing Co"
+    ])
+    def test_brewery_by_name(self, name, name_url):
         """
         Тест проверяет по выбранному имени пивоварни
         """
-        response_brewery_name = requests.get(breweries_url).json()
-        if name in response_brewery_name[0]['name']:
-            print(f'Name: {name}')
-        elif name not in response_brewery_name[0]['name']:
-            assert False, f'Name: {name}'
-        else:
-            assert False, 'Something wrong'
+        response_brewery_name = requests.get(name_url + f'{name}').json()
 
-    @pytest.mark.parametrize('city', [
-        "Tucson", "Lake Havasu City", "Chandler", "Phoenix", "Paragould"])
-    def test_brewery_by_city(self, city):
-        """
-        Тест проверяет по выбранному городу
-        """
-        response_brewery_city = requests.get(breweries_url).json()
-        if city in response_brewery_city[0]['city']:
-            print(f'City: {city}')
-        elif city not in response_brewery_city[0]['city']:
-            assert False, f'City: {city}'
-        else:
-            assert False, 'Something wrong'
+        # Проверяем, что {name} есть в ответе на запрос в поле 'name'
+        assert name in response_brewery_name[0]['name']
 
-    @pytest.mark.parametrize('tag', ["Patio"])
+    @pytest.mark.parametrize('brewery_id, phone', [
+        ("1", "2516897483"),
+        ("50", "9073605104"),
+        ("100", "9284422337"),
+        ("250", "4796362337"),
+        ("500", "6192752215")
+    ])
+    def test_brewery_by_city(self, brewery_id, phone):
+        """
+        Проверяем, что {brewery_id} есть в ответе на запрос в поле 'id'
+        И что вы выбранном {brewery_id} есть заданный {phone}
+        """
+        response_brewery_id = \
+            requests.get(breweries_url + f'/{brewery_id}').json()
+        assert \
+            brewery_id == str(response_brewery_id['id']) and\
+            phone == response_brewery_id['phone']
+
+    @pytest.mark.parametrize('tag', ["patio"])
     def test_brewery_by_tag(self, tag, response_brewery_by_tag):
         """
         Тест проверяет по выбранному тегу
         """
-        if tag in response_brewery_by_tag[0]['tag_list']:
-            print(f'Tag: {tag}')
-        elif tag not in response_brewery_by_tag[0]['tag_list']:
-            assert False, f'Tag: {tag}'
-        else:
-            assert False, 'Something wrong'
+        assert tag in response_brewery_by_tag[0]['tag_list']
