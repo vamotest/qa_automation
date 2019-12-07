64c9ff614de47dee83e47ae8a37d2af3d16c8000
Valery Moiseev <vamotest@gmail.com>
2019-12-07 17:36:57 +0300

    Remove browser logic

--- 05_11_Selenium/conftest.py
+++ 05_11_Selenium/conftest.py
@@ -1,5 +1,5 @@
+from fixtures.browser import Browser
 import pytest
-from selenium import webdriver
 
 
 def pytest_addoption(parser):
@@ -12,47 +12,18 @@ def pytest_addoption(parser):
     )
 
     parser.addoption(
-        "--opencart_url",
+        "--url",
         action="store",
-        default="http://localhost:80/",
+        default="http://127.0.0.1:80/",
         help="This is opencart_url",
         required=False
     )
 
 
-@pytest.fixture()
-def browser_param(request):
-    return request.config.getoption("--browser")
-
-
-@pytest.fixture()
-def opencart_url_param(request):
-    return request.config.getoption('--opencart_url')
-
-
-@pytest.fixture()
-def chosen_driver(browser_param):
-    if browser_param == "Chrome":
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless")
-        driver = webdriver.Chrome(options=options)
-        driver.maximize_window()
-        return driver
-    elif browser_param == "Firefox":
-        options = webdriver.FirefoxOptions()
-        options.add_argument("--headless")
-        driver = webdriver.Firefox(options=options)
-        driver.maximize_window()
-        return driver
-    elif browser_param == "Safari":
-        """
-        У Safari нет headless-режима
-        """
-        driver = webdriver.Safari()
-        driver.maximize_window()
-        return driver
-
-
-@pytest.fixture()
-def opencart_url(opencart_url_param):
-    return opencart_url_param
+@pytest.fixture(scope="session")
+def browser(request):
+    url = request.config.getoption("--url")
+    browser = request.config.getoption("--browser")
+    fixture = Browser(url=url, browser=browser)
+    yield fixture
+    fixture.quit()
