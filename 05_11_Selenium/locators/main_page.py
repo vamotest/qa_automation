aff40c1a375c9b842ef7f952b60803155ac330f6
Valery Moiseev <vamotest@gmail.com>
2019-12-12 23:01:02 +0300

    Add MainPage

--- 05_11_Selenium/locators/main_page.py
+++ 05_11_Selenium/locators/main_page.py
@@ -9,5 +9,3 @@ class MainPage:
     # Поисковая строка и кнопка:
     search_string = (By.CSS_SELECTOR, '[class="form-control input-lg"]')
     search_button = (By.CSS_SELECTOR, '[class="btn btn-default btn-lg"]')
-
-
