3e5ca616b89e017039cd91a4c5db8228d1e3ff1d
Valery Moiseev <vamotest@gmail.com>
2020-01-25 19:55:53 +0300

    Add banner's elements

--- 05_11_Selenium/locators/main_page.py
+++ 05_11_Selenium/locators/main_page.py
@@ -9,3 +9,10 @@ class MainPage:
     # Поисковая строка и кнопка:
     search_string = (By.CSS_SELECTOR, '[class="form-control input-lg"]')
     search_button = (By.CSS_SELECTOR, '[class="btn btn-default btn-lg"]')
+
+    # Banner elements:
+    next_banner = (By.XPATH, "//div[@id='slideshow0']/parent::div//div[@class='swiper-button-next']")
+    previous_banner = (By.XPATH, "//div[@id='slideshow0']/parent::div//div[@class='swiper-button-prev']")
+
+    iphone_banner = (By.CSS_SELECTOR, "img[alt='iPhone 6']")
+    macbook_banner = (By.CSS_SELECTOR, "img[alt='MacBookAir']")
