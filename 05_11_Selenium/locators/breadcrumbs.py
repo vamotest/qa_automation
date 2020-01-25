0e3124203e1910de351f3246890b424ec5f9a37c
Valery Moiseev <vamotest@gmail.com>
2020-01-25 18:54:21 +0300

    Change selector names

--- 05_11_Selenium/locators/breadcrumbs.py
+++ 05_11_Selenium/locators/breadcrumbs.py
@@ -3,5 +3,5 @@ from selenium.webdriver.common.by import By
 
 class BreadCrumbs:
 
-    account_breadcrumb = (By.CSS_SELECTOR, '#account-account > ul li:nth-child(2)')
-    logout_breadcrumb = (By.CSS_SELECTOR, '.breadcrumb li:nth-child(3)')
+    account = (By.CSS_SELECTOR, '#account-account > ul li:nth-child(2)')
+    logout = (By.CSS_SELECTOR, '.breadcrumb li:nth-child(3)')
