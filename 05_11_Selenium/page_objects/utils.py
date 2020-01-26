63d458616fd0fa0e864ff5dbc2fcc6e561c04fd9
Valery Moiseev <vamotest@gmail.com>
2020-01-26 18:25:31 +0300

    Fix errors with names

--- 05_11_Selenium/page_objects/utils.py
+++ 05_11_Selenium/page_objects/utils.py
@@ -11,11 +11,11 @@ class Utils:
 
     def accept_alert(self):
         Alert(self.driver).accept()
-        self.wait_success_alert()
+        self.wait_alert_success()
 
-    def wait_success_alert(self):
+    def wait_alert_success(self):
         WebDriverWait(self.driver, 5).until(
             EC.presence_of_element_located(
-                AdminPage.Products.success_alert
+                AdminPage.Products.AddProduct.alert_success
             )
         )
