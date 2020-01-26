d7e7c8cee33312cadeb1c566ce3144093b9bcbba
Valery Moiseev <vamotest@gmail.com>
2020-01-26 16:24:07 +0300

    Change AdminPage PageObject

--- 05_11_Selenium/page_objects/admin_page.py
+++ 05_11_Selenium/page_objects/admin_page.py
@@ -18,3 +18,36 @@ class AdminPage:
     def logout(self):
         button = self.driver.find_element(*admin_page.logout)
         button.click()
+
+    def fill_product_name(self, product_name):
+        field = self.driver.find_element(
+            *admin_page.Products.AddProduct.product_name)
+        field.clear()
+        field.send_keys(product_name)
+
+    def fill_meta_tag_title(self, meta_tag_title):
+        field = self.driver.find_element(
+            *admin_page.Products.AddProduct.meta_tag_title)
+        field.clear()
+        field.send_keys(meta_tag_title)
+
+    def click_navigation_data(self):
+        navigation_data = self.driver.find_element(
+            *admin_page.Products.AddProduct.navigation_data)
+        navigation_data.click()
+
+    def fill_model(self, model):
+        field = self.driver.find_element(*admin_page.Products.AddProduct.model)
+        field.clear()
+        field.send_keys(model)
+
+    def fill_price(self, price):
+        field = self.driver.find_element(*admin_page.Products.AddProduct.price)
+        field.clear()
+        field.send_keys(price)
+
+    def fill_min_quantity(self, min_quantity):
+        field = self.driver.find_element(
+            *admin_page.Products.AddProduct.min_quantity)
+        field.clear()
+        field.send_keys(min_quantity)
