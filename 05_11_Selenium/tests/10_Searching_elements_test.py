0ad9d2ecaa5863e01f7361217022c685f7438d62
Valery Moiseev <vamotest@gmail.com>
2020-01-26 18:29:22 +0300

    Add test_next_banner

--- 05_11_Selenium/tests/10_Searching_elements_test.py
+++ 05_11_Selenium/tests/10_Searching_elements_test.py
@@ -144,3 +144,23 @@ def test_remove_from_cart(browser):
     # Проверяем, что товар был успешно удален:
     result = product_page.delete_from_cart_result()
     assert result == 'Your shopping cart is empty!'
+
+
+@pytest.mark.parametrize('banner', ["iphone", "macbook"])
+def test_next_banner(browser, banner):
+    """
+    Проверка переключения баннера
+    :param browser:
+    :param banner:
+    :return:
+    """
+    main_page = MainPage(browser.wd)
+
+    # Открываем главную страницу:
+    browser.open_main_page()
+
+    # Переключаем на следующий баннер:
+    main_page.switch_next_banner()
+
+    # Проверяем текущий баннер:
+    main_page.check_banner_is_present(banner)
