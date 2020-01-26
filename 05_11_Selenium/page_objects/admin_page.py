140b7544598e203504fd3b47c59fe93e5d47baae
Valery Moiseev <vamotest@gmail.com>
2020-01-26 15:41:46 +0300

    Change AdminPage PageObject

--- 05_11_Selenium/page_objects/admin_page.py
+++ 05_11_Selenium/page_objects/admin_page.py
@@ -6,6 +6,15 @@ class AdminPage:
     def __init__(self, driver):
         self.driver = driver
 
+    def click_catalog(self):
+        catalog = self.driver.find_element(*admin_page.Navigation.catalog)
+        catalog.click()
+
+    def click_products(self):
+        products = self.driver.find_element(
+            *admin_page.Navigation.CatalogMenu.products)
+        products.click()
+
     def logout(self):
         button = self.driver.find_element(*admin_page.logout)
         button.click()
