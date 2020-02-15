3c6ccf64a6013d5d89e9aa75979a87399a221d7a
Valery Moiseev <vamotest@gmail.com>
2020-02-15 19:19:37 +0300

    Add MySQL DB connection fixture

--- 05_11_Selenium/conftest.py
+++ 05_11_Selenium/conftest.py
@@ -1,4 +1,5 @@
 from fixtures.browser import Browser
+from fixtures.database import DataBase
 import pytest
 
 
@@ -33,6 +34,28 @@ def browser(request):
     url = request.config.getoption("--url")
     browser = request.config.getoption("--browser")
     implicitly_wait = request.config.getoption("--implicitly_wait")
-    fixture = Browser(url=url, browser=browser, implicitly_wait=implicitly_wait)
+    fixture = Browser(url=url, browser=browser,
+                      implicitly_wait=implicitly_wait)
     yield fixture
     fixture.quit()
+
+
+@pytest.fixture(scope="session")
+def database():
+    db_config = {
+        'host': 'localhost',
+        'name': 'bitnami_opencart',
+        'user': 'bn_opencart',
+        'password': '',
+        'port': 3306
+    }
+
+    db_fixture = DataBase(
+        host=db_config['host'],
+        name=db_config['name'],
+        user=db_config['user'],
+        password=db_config['password'],
+        port=db_config['port'])
+
+    yield db_fixture
+    db_fixture.destroy()
